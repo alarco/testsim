33092c90d6f9449d8021ba13105accafa5aeabcc
Alicia Arenas <arenasalic@gmail.com>
2019-11-17 22:09:31 +0100

    Añadimos el comando para descomprimir el genoma

--- scripts/analyse_sample.sh
+++ scripts/analyse_sample.sh
@@ -2,8 +2,9 @@
 #Previamente se ha tenido que preparar el ambiente en conda, 
 #instalar los programas fastqc, seqtk, cutadapt, STAR, multiqc
 
-# Download the E.coli genome in the $WD/res/genome
+# Download the E.coli genome in the $WD/res/genome y lo descomprimimos
 wget -O $WD/res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
+gunzip -k $WD/res/genome/ecoli.fasta.gz
 
 #Creación de los directorios necesarios para el analisis.
 mkdir -p out/fastqc original res/genome/star_index out/cutadapt log/cutadapt 
