56fc5ce38fda79c280e4627c87d53987b01218d7
Alicia Arenas <arenasalic@gmail.com>
2019-11-17 20:27:46 +0100

    Eliminación de los mkdir dentro del loop

--- scripts/analyse_sample.sh
+++ scripts/analyse_sample.sh
@@ -6,7 +6,7 @@
 wget -p $WD/res/genome -o ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
 
 #Creación de los directorios necesarios para el analisis.
-mkdir -p out/fastqc original res/genome/star_index out/cutadapt log/cutadapt
+mkdir -p out/fastqc original res/genome/star_index out/cutadapt log/cutadapt 
 
 
 
@@ -14,16 +14,12 @@ if [ "$#" -eq 1 ]
 then
     sampleid=$1
     echo "Running FastQC..."
-    mkdir -p out/fastqc
     fastqc -o out/fastqc data/${sampleid}*.fastq.gz
     echo
     echo "Running cutadapt..."
-    mkdir -p log/cutadapt
-    mkdir -p out/cutadapt
     cutadapt -m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT -o out/cutadapt/${sampleid}_1.trimmed.fastq.gz -p out/cutadapt/${sampleid}_2.trimmed.fastq.gz data/${sampleid}_1.fastq.gz data/${sampleid}_2.fastq.gz > log/cutadapt/${sampleid}.log
     echo
     echo "Running STAR index..."
-    mkdir -p res/genome/star_index
     STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/genome/star_index/ --genomeFastaFiles res/genome/ecoli.fasta --genomeSAindexNbases 9
     echo
     echo "Running STAR alignment..."
