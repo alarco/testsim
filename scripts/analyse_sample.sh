38202275f3e84b6d71b8511f83a07cc42456376b
Alicia Arenas <arenasalic@gmail.com>
2019-11-17 20:22:45 +0100

    Creación de directorios de salida de resultados, logs, errores...

--- scripts/analyse_sample.sh
+++ scripts/analyse_sample.sh
@@ -5,6 +5,10 @@
 # Download the E.coli genome in the $WD/res/genome
 wget -p $WD/res/genome -o ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
 
+#Creación de los directorios necesarios para el analisis.
+mkdir -p out/fastqc original res/genome/star_index out/cutadapt log/cutadapt
+
+
 
 if [ "$#" -eq 1 ]
 then
